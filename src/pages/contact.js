bf90fe540bcd5efaab2bf2183614b002755f649e
Alexander Kohlhofer <3952+kohlhofer@users.noreply.github.com>
2019-12-16 09:11:49 -0500

    added a contact form via Kwes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,14 +6,29 @@ import SEO from "../components/seo"
 
 const contactPage = () => (
   <Layout pageTitle="Contact">
-	<div className="contactPage">
-<p>I have extensive experience working and collaborating remotely. I lived and worked on both coasts of the US (and the middle), in the UK, Austria, and Germany.</p>
+	<div className="contact">
 
+<div className="kwes-form"> 
+    <form method="POST" success-message="Thank you for your message!" error-message="Looks like there was an error. Please check the form" action="https://kwes.io/api/foreign/forms/TdKsRlv5KLE2nOzg3yC6"> 
+<h1>Get in touch</h1>
+<p>
+        <label for="email">Your email</label> 
+        <input type="text" name="email" rules="required|email|max:255"/> 
+</p>
+<p>
+        <label for="message">Your message</label> 
+        <textarea rows="4" type="text" name="message" rules=""/> 
+
+</p>
+<p>
+        <button type="submit">Submit</button> 
+</p>
+    </form> 
+</div>
 <hr />
-
-<p>Get in touch:</p>
-<p>alexander@kohlhofer.com</p>
-
+<div className="contact-words">
+<p>I have extensive experience working and collaborating remotely. I lived and worked on both coasts of the US (and the middle), in the UK, Austria, and Germany.</p>
+</div>
 
 </div>  
 </Layout>
